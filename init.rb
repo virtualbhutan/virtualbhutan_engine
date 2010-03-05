ac3b0fb1da4ec9cf8eb63564a9006410df7224eb
Andres Montano <amontano@virginia.edu>
2010-03-05 16:57:50 +0600

    fixed active resource

--- init.rb
+++ init.rb
@@ -1,2 +1,6 @@
 # Include hook code here
+require 'hpricot'
+require 'patches/traverse'
+require 'patches/active_resource'
 ActionView::Base.send :include, BasicEngineHelper
+ActionView::Base.send :include, TinyMceExtensionHelper
